5b10c9aeee35c525ed548640825c310ae593729f
srinathreddy0609 <114682789+srinathreddy0609@users.noreply.github.com>
2023-06-23 19:50:22 +0530

    Add files via upload

---
+++ firstclass.py
@@ -0,0 +1,23 @@
+
+class Phone:
+    def make_call(self):
+        print("making call with other phone")
+    def play_game(self):
+        print("playing games")
+    def set_color(self,color):
+        self.color=color
+    def show_color(self):
+        print(self.color)
+class miphone(Phone):
+    name="redmi 4A"
+    def make_call(self):
+        print("making call with mi phone")
+
+p1=miphone()
+p1.make_call()
+p1.play_game()
+p1.set_color("blue")
+p1.show_color()
+p2=Phone()
+p2.make_call()
+print(p1.name)
