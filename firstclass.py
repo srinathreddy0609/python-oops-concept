cd84ed30a8311d91bbad29d31e68f62c7745a314
srinathreddy0609 <114682789+srinathreddy0609@users.noreply.github.com>
2023-06-23 20:03:10 +0530

    Update firstclass.py

--- firstclass.py
+++ firstclass.py
@@ -1,24 +1,10 @@
-# class is a blue print for creating an objects
-# class does not take memory space
-class Phone:
-    def make_call(self):
-        print("making call with other phone")
-    def play_game(self):
-        print("playing games")
-    def set_color(self,color):
-        self.color=color
-    def show_color(self):
-        print(self.color)
-class miphone(Phone):
-    name="redmi 4A"
-    def make_call(self):
-        print("making call with mi phone")
+class phone:                       #creating class
 
-p1=miphone()
-p1.make_call()
+    def make_call(self):           
+        print("making phone call")
+    def play_game(self):
+        print("playing game")
+
+p1=phone()                         #instantiating the p1 pbject
+p1.make_call()                     #invoking methods through object
 p1.play_game()
-p1.set_color("blue")
-p1.show_color()
-p2=Phone()
-p2.make_call()
-print(p1.name)
